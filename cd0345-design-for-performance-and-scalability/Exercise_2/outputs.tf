1882f6c94db50fb56b0f99009097b4ab304ff05e
Mohammed Ghawanni <ghawanni.m@gmail.com>
2023-01-06 18:37:33 +0300

    Revert "Revert "pushing all project files for submission""
    This reverts commit 3c8b2e744abdb21e52d2818f9d915840c6ae9078.

---
+++ cd0345-design-for-performance-and-scalability/Exercise_2/outputs.tf
@@ -0,0 +1,5 @@
+# TODO: Define the output variable for the lambda function.
+output "lambda_output" {
+  description = "Output for the lambda function"
+  value       = aws_lambda_function.lambda_for_udacity_project
+}
